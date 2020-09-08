c522b82dc7a98e96d39af5801578a980ec23d042
Gleb Tishchenko <owod1.0@gmail.com>
2020-09-08 14:14:44 +0300

    Initial Commit

---
+++ MultiTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiTimer
+//
+//  Created by GLEB TISHCHENKO on 8.9.2020.
+//  Copyright © 2020 GLEB TISHCHENKO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
